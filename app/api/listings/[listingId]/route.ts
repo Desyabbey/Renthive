605bb507a958857078720a6bad6b86d6474902e9
Abbey Desmond <abbeydesmond1225@gmail.com>
2024-04-13 08:39:36 +0100

    first commit

--- app/api/listings/[listingId]/route.ts
+++ app/api/listings/[listingId]/route.ts
@@ -11,7 +11,7 @@ export async function DELETE(
     request: Request,
     { params }: { params: IParams }
 ) {
-    const currentUser =await getCurrentUser();
+    const currentUser = await getCurrentUser();
 
     if (!currentUser) {
         return NextResponse.error
